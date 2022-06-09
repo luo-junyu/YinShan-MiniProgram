e617b7a48e8d6a6788e1bac320a1984a3ce21060
hxx <hxx0215@126.com>
2022-06-08 21:05:36 -0400

    [feat] add next action loading step support

--- pages/login/login.js
+++ pages/login/login.js
@@ -7,8 +7,6 @@ Page({
     // sLoged: ''
     callback: '',
     checked: false,
-    bUserModal: false, // 显示modal弹窗
-    bSecretModal: false, // 显示modal弹窗
     single: true // false 只显示一个按钮，如果想显示两个改为true即可
   },
   phoneCode: '',
@@ -96,25 +94,14 @@ Page({
     this.setData({ checked: !this.data.checked })
   },
   showUserContract () {
-    this.setData({ bUserModal: true })
+    wx.navigateTo({
+      url: '/pages/agreement/agreement'
+    })
   },
   showSecretContract () {
-    this.setData({ bSecretModal: true })
-  },
-  // 点击取消按钮的回调函数
-
-  modalCancel (e) {
-    // 这里面处理点击取消按钮业务逻辑
-
-    console.log('点击了取消')
-  },
-
-  // 点击确定按钮的回调函数
-
-  modalConfirm (e) {
-    // 这里面处理点击确定按钮业务逻辑
-
-    console.log('点击了确定')
+    wx.navigateTo({
+      url: '/pages/agreement/privacy'
+    })
   },
   gotoNext () {
     wx.switchTab({
